8be19459fbfe560009ac5d2336865ff5eea3dd0f
Maloney L <maloneyl@users.noreply.github.com>
2018-04-13 11:57:27 +0100

    add routes (note 'exact')

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,30 @@ import React, { Component } from 'react'
 import Home from './Home'
 import About from './About'
 import Topics from './Topics'
+import { // all these we're importing are components
+  BrowserRouter,
+  Route,
+  Link
+} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello, World
-        <Home />
-        <About />
-        <Topics />
-      </div>
+      <BrowserRouter>
+        <div>
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/topics'>Topics</Link></li>
+          </ul>
+
+          <hr />
+
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/topics' component={Topics} />
+        </div>
+      </BrowserRouter>
     )
   }
 }
