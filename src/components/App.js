5536169f8373eded1cd136304733bb0f56cfa690
Maloney L <maloneyl@users.noreply.github.com>
2018-04-13 11:37:46 +0100

    add a few components

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Home from './Home'
+import About from './About'
+import Topics from './Topics'
 
 class App extends Component {
   render() {
     return (
       <div>
         Hello, World
+        <Home />
+        <About />
+        <Topics />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
